71bf6a8c5f48b03f81d91fd0dd1389becc9956fc
Brennan Lu <zh4lu@uwaterloo.ca>
2019-12-02 20:58:41 -0500

    Added first Dijit onto index.html

--- templates/js-module/views/controls/ComponentA.js
+++ templates/js-module/views/controls/ComponentA.js
@@ -1,12 +1,18 @@
 define([
+    "dojo/_base/declare",
     "dijit/_WidgetBase",
     "dijit/_TemplatedMixin",
     "dojo/text!./ComponentAViewTemplate.html",
-    "dojo/domReady"
-], (_WidgetBase, _TemplatedMixin, template) => {
+    "dojo/domReady!"
+], (declare, _WidgetBase, _TemplatedMixin, template) => {
     return declare([_WidgetBase, _TemplatedMixin], {
         templateString: template,
+        counter: null,
 
-        constructor: function() { }
+        constructor: function() { },
+
+        onOkClick: function() {
+            this.counter.textContent = parseInt(this.counter.textContent) + 1;
+        }
     });
 });
