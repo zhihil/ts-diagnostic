e0fefd5e9cad85b7a2fd9ecbf95bfddbe8c17612
Brennan Lu <zh4lu@uwaterloo.ca>
2019-12-04 20:10:51 -0500

    Updated script to download uncompressed Dojo src if specified --debug

--- get-dojo.sh
+++ get-dojo.sh
@@ -1,14 +1,26 @@
 
 if [ $# -eq 1 ] && [ $1 = '--debug' ] 
 then
-  curl 'http://download.dojotoolkit.org/release-1.16.0/dojo-release-1.16.0-src.tar.gz' --output dojo.tar.gz
+  echo "Getting uncompressed source Dojo"
+  curl 'https://download.dojotoolkit.org/release-1.16.0/dojo-release-1.16.0-src.tar.gz' --output dojo.tar.gz
 else
+  echo "Getting compressed release version Dojo"
   curl 'https://download.dojotoolkit.org/release-1.16.0/dojo-release-1.16.0.tar.gz' --output dojo.tar.gz
 fi
 
 tar -xzf dojo.tar.gz
-mv 'dojo-release-1.16.0/dojo' .
-mv 'dojo-release-1.16.0/dijit' .
-mv 'dojo-release-1.16.0/dojox' .
+
+if [ $# -eq 1 ] && [ $1 = '--debug' ] 
+then
+  mv 'dojo-release-1.16.0-src/dojo' .
+  mv 'dojo-release-1.16.0-src/dijit' .
+  mv 'dojo-release-1.16.0-src/dojox' .
+  rm -rf 'dojo-release-1.16.0-src'
+else
+  mv 'dojo-release-1.16.0/dojo' .
+  mv 'dojo-release-1.16.0/dijit' .
+  mv 'dojo-release-1.16.0/dojox' .
+  rm -rf 'dojo-release-1.16.0'
+fi
+
 rm dojo.tar.gz
-rm -rf 'dojo-release-1.16.0'
