77478ba37dc4dcb09ca4c0126b30b2cf57a232f3
Brennan Lu <zh4lu@uwaterloo.ca>
2019-12-04 19:53:54 -0500

    Modified script to be more accurate in its file extensions

--- get-dojo.sh
+++ get-dojo.sh
@@ -1,14 +1,14 @@
 
 if [ $# -eq 1 ] && [ $1 = '--debug' ] 
 then
-  curl 'http://download.dojotoolkit.org/release-1.16.0/dojo-release-1.16.0-src.tar.gz' --output dojo.zip
+  curl 'http://download.dojotoolkit.org/release-1.16.0/dojo-release-1.16.0-src.tar.gz' --output dojo.tar.gz
 else
-  curl 'https://download.dojotoolkit.org/release-1.16.0/dojo-release-1.16.0.tar.gz' --output dojo.zip
+  curl 'https://download.dojotoolkit.org/release-1.16.0/dojo-release-1.16.0.tar.gz' --output dojo.tar.gz
 fi
 
-tar -xzf dojo.zip
+tar -xzf dojo.tar.gz
 mv 'dojo-release-1.16.0/dojo' .
 mv 'dojo-release-1.16.0/dijit' .
 mv 'dojo-release-1.16.0/dojox' .
-rm dojo.zip
+rm dojo.tar.gz
 rm -rf 'dojo-release-1.16.0'
