08efae7e7396617bf3a26daddad3fed36737392f
Brennan Lu <zh4lu@uwaterloo.ca>
2019-12-04 20:23:26 -0500

    Test commit - changed ViewModels

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const { readFile } = require("fs");
+const cors = require('cors');
+
+const express = require("express");
+const app = express();
+const port = 9001;
+
+app.use(cors());
+
+app.get('/users', (_, res) => {
+    readFile('./users.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        res.send(JSON.parse(data));
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Backend server serving on port ${port}`)
+});
---
+++ frontend/js-module/models/ProfileCollectionViewModel.js
@@ -0,0 +1,40 @@
+define([
+  'dojo/_base/declare',
+  'dojo/Stateful',
+  'dojo/request',
+  'models/ProfileViewModel'
+], (declare, Stateful, request, ProfileViewModel) => {
+  return declare([Stateful], {
+      userProfiles: null,
+      usersUrl: 'http://localhost:9001/users',
+
+      isFetching: false,
+
+      constructor() {
+          this.getUsersData();
+      },
+
+      getUsersData() {
+          this.isFetching = true;
+          request(this.usersUrl).then(
+              this.getUsersSuccess,
+              this.getUsersFailed
+          );
+      },
+
+      getUsersSuccess(data) {
+          const parsedData = JSON.parse(data);
+
+          this.userProfiles = {};
+          for (const id in parsedData) {
+              this.userProfiles[id] = new ProfileViewModel(parsedData[id]); 
+          }
+
+          this.isFetching = false;
+      },
+
+      getUsersFailed(_err) {
+          this.isFetching = false;
+      }
+  });
+});
