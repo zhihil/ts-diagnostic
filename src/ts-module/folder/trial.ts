4f13f63f700a8c03fa11d459eb227bbcb23e5dc9
Brennan Lu <zh4lu@uwaterloo.ca>
2019-12-07 14:09:25 -0500

    dev/configure-ts-build - added a tsc build script and target folder for compiled .js

---
+++ src/ts-module/folder/trial.ts
@@ -0,0 +1,3 @@
+export function printEconomics() {
+  console.log("Economics");
+}
--- src/ts-module/main.ts
+++ src/ts-module/main.ts
@@ -1,1 +1,13 @@
+import { printEconomics } from "./folder/trial";
 
+interface Student {
+  name: string;
+  age: number;
+}
+
+const obj: Student = {
+  name: "Adam Smith",
+  age: 42
+};
+
+printEconomics();
