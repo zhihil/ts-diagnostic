a0794aaf913ac45cfd88610a32f9c74b797b9a93
Brennan Lu <zh4lu@uwaterloo.ca>
2019-12-07 13:55:17 -0500

    dev/configure-ts-build - Giant commit to recover git history from lost branch

---
+++ frontend/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("../static"));
+
+app.listen(8000, () => {
+    console.log(`Express server serving ${process.argv[2]} on port ${8000}`)
+});
--- src/frontend/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.use(express.static("js-module"));
-
-app.listen(8000, () => {
-    console.log(`Express server serving ${process.argv[2]} on port ${8000}`)
-});
