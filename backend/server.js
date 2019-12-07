4f23afe4cb620250b565129f75dafac5ea788936
Brennan Lu <zh4lu@uwaterloo.ca>
2019-12-06 20:42:23 -0500

    comments/endpoint - added new endpoint for retrieving comments associated with a specific user

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,47 @@
 const { readFile } = require("fs");
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const express = require("express");
 const app = express();
 const port = 9001;
 
-app.use(cors());
+function filterByTargetUserQuery(userId) {
+    return function _filterByTargetUserQuery(data) {
+        const result = {};
+        for (const id in data) {
+            if (data[id].targetUserId === userId) {
+                result[id] = data[id];
+            }
+        }
+        return result;
+    }
+}
 
+function executeQuery(data, queryFunc) {
+    return queryFunc(data);
+}
+
+app.use(cors());
+app.use(bodyParser.json());
+
+/* GET all user infomration */
 app.get('/users', (_, res) => {
     readFile('./users.json', 'utf8', (err, data) => {
         if (err) throw err;
         res.send(JSON.parse(data));
+    });
+});
+
+/* GET the comments posted to a given target user */
+app.get('/comments', (req, res) => {
+    readFile('./comments.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        const filteredData = executeQuery(
+            JSON.parse(data), 
+            filterByTargetUserQuery(req.body.userId)
+        );
+        res.send(filteredData);
     });
 });
 
