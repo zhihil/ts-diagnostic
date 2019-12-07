2f94d874450b4b8329059f304c7a0abbcee9e2e5
Brennan Lu <zh4lu@uwaterloo.ca>
2019-12-07 13:58:37 -0500

    dev/configure-ts-build - corrected frontend server.js

--- frontend/server.js
+++ frontend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("../static"));
+app.use(express.static("static"));
 
 app.listen(8000, () => {
     console.log(`Express server serving ${process.argv[2]} on port ${8000}`)
